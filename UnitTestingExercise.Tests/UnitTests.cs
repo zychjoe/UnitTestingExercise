f396a0099eb0f5f934d1680ca849d2c2b142a17b
Joe Zedd <zychjoe@gmail.com>
2022-02-08 18:52:34 -0500

    initial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,102 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(0, 0, 3, 3)]
+        [InlineData(-1, -8, 9, 0)]
+        [InlineData(400, 40, 4, 444)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var methods = new UnitTestMethods();
+
 
             //Act
+            var actual = methods.Add(num1, num2, num3);
 
             //Assert
+            Assert.Equal(actual, expected);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(100, 50, 50)]
+        [InlineData(100, -50, 150)]
+        [InlineData(10, 10, 0)]
+        [InlineData(0, 50, -50)]
+        [InlineData(10, 0, 10)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var methods = new UnitTestMethods();
 
             //Act
+            var actual = methods.Subtract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(1 , 5, 5)]
+        [InlineData(10, 0, 0)]
+        [InlineData(0, 5, 0)]
+        [InlineData(10, 1, 10)]
+        [InlineData(10, 5, 50)]
+        [InlineData(10, 50, 500)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var methods = new UnitTestMethods();
 
             //Act
+            var actual = methods.Multiply(num1, num2);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5, 5, 1)]
+        [InlineData(100, 10, 10)]
+        [InlineData(125, 5, 25)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var methods = new UnitTestMethods();
 
             //Act
+            var actual = methods.Divide(num1, num2);
 
             //Assert
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void IsTrueCodersAwesome()
+        {
+            //Arrange
+            var methods = new UnitTestMethods();
+
+            //Act
+            var actual = methods.IsTrueCodersAwesome();
+
+            //Assert
+            Assert.Equal(true, actual);
 
         }
 
         [Fact]
-        public void YourMethodName()
+        public void GiveMeAJ()
         {
             //Arrange
+            var methods = new UnitTestMethods();
 
             //Act
+            var actual = methods.GiveMeAJ();
 
             //Assert
-
-        }
-
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
+            Assert.Equal("J", actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -10,17 +10,45 @@ namespace UnitTestingExercise
 
         // Create an Add method that passes 3 integers
 
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
 
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
         // Create a Multiply method that passes 2 integers
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
 
         // Create a Divide method that passes 2 integers
 
-
+        public int Divide(int dividend, int divisor)
+        {
+            return dividend / divisor;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+        public bool IsTrueCodersAwesome()
+        {
+            return true;
+        }
+
+        public string GiveMeAJ()
+        {
+            return "J";
+        }
+
+
     }
 }
